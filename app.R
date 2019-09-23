04f4981a888dce4a24b21f3df9498d2ed33c462a
hahaha447 <40543217+hahaha447@users.noreply.github.com>
2019-09-23 13:33:46 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,25 +1,33 @@
 library(shiny)
 library(ggplot2)
 ui <- fluidPage(
-  titlePanel("Financial Planning"),
+  titlePanel("Withdrawal amount calculation"),
   sidebarLayout(
     sidebarPanel(
       
       numericInput("initial_asset",label = h3( "Initial asset:"),value = 5000),
       sliderInput("Time",label = h3("End Time:"),min = 0,max = 100,value = 30),
       sliderInput("Age",label = h3("Start age/end age:"),min = 0,max = 110,value = c(30,60)),
-      numericInput("mu",label = h3( "mu:"),value = 0.0671,step = 0.0001),
-      numericInput("sig",label = h3( "sig:"),value = 0.1524,step = 0.0001)
+      numericInput("quantile",label = h3("Quantile"),value = 0.05,step = 0.01),
+      numericInput("threshold",label = h3("Threshold"),value = 0,step = 100),
+      br(),
+      h2("parameters of return matrix"),
+      numericInput("mu",label = h3( "mu:"),value = 0.04,step = 0.01),
+      numericInput("sig",label = h3( "sig:"),value = 0.1,step = 0.01),
+      sliderInput("n_sim",label = h3("number of simulation"),min = 0,max = 2000,step = 100,value = 1000)
       
     ),
     mainPanel(
+      h3("Remaining"),
+      plotOutput("remaining"),
       h3("Histogram for D0"),
       plotOutput("myhist"),
    
       textOutput("runtime"),
+      h3(textOutput("quantile")),
     
       textOutput("Mean"),
-      h3("Dataframe of all D0s:"),
+      h5("Dataframe of all D0s:"),
       dataTableOutput("d0")
       
     )
@@ -33,12 +41,13 @@ ui <- fluidPage(
 
 
 server <- function(input,output){
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   Generate_scenario <- reactive({
     start_age <- input$Age[1]
     end_age <- input$Age[2]
     mu <- input$mu
     sig <- input$sig
-    n_sim <- 1000
+    n_sim <- input$n_sim
     
     #
     duration <- end_age - start_age
@@ -47,35 +56,55 @@ server <- function(input,output){
     rset <- exp(mu - 0.5 * sig^2) * exp(sig)^(Z)
     return(rset)
   })
-  
-  myfun <- function(r_vec,initial_asset,I,EndTime=30){
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  myfun <- function(r_vec,initial_asset,I=rep(1,30),EndTime=30,ending_value=0,cashflow=rep(0,50)){
     I[1] <- 1
+    cashflow_endvalue <- cashflow
     x <- prod(initial_asset,r_vec[1:EndTime])
     a <- rep(0,EndTime)
+    b <- rep(0,EndTime)
     for (k in 1:EndTime) {
       a[k] <- prod(r_vec[k:EndTime],I[1:k])
+      b[k] <- prod(cashflow[k],r_vec[k:EndTime],1/I[1:k])
     }
     y <- sum(a)
-    d0 <- x/y
-    
+    d0 <- (x+sum(b)-ending_value)/y
     return(d0)
   }
-    
-  
-  I1 <- 1+rnorm(1000,0.03,0.001)
+  #----------------------------------------------------------------------------------------------------
+  calculate_d0_NR <-  function(n_year,quantile=0.05,S0,real_return,d0_vec){
+    n <- quantile*length(d0_vec)
+    d0_optimal <- sort(d0_vec)[n]
+    d0_result <- rep(d0_optimal,n_year)
+    remaining_result <- rep(S0,n_year)
+    for (i in 1:n_year) {
+      S0 <- (S0-d0_optimal)*real_return[i] 
+      remaining_result[i] <- S0
+    }
+    return(list("Withdrwal"=d0_result,"Remaining_value"=remaining_result))
+  }
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  I1 <- reactive({I <- 1+rnorm(Generate_scenario(),0.00,0.000)
+                 return(I)})
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   df <- reactive({
+    I1 <- I1()
+    R <- Generate_scenario()
     t1 <- Sys.time()
-    df <- apply(X=Generate_scenario(),2,FUN = myfun,initial_asset=input$initial_asset,
-                I=I1,EndTime=input$Time)
+    df <- apply(X=R,2,FUN = myfun,initial_asset=input$initial_asset,
+                I=I1,EndTime=input$Time,ending_value=input$threshold)
     t2 <- Sys.time()
     runtime <- t2-t1
-    meanvalue <- mean(df)
+    actural_return <- R[,which(order(colSums(R))==input$n_sim/2)]
+    
+    quantile <- calculate_d0_NR(n_year = input$Time,quantile=input$quantile,S0 = input$initial_asset,real_return =actural_return,d0_vec = df)
     df <- data.frame(df)
     colnames(df) <- "D0"
-    
-    return(list(df,runtime,meanvalue))
+    return(list(df,runtime,quantile$Withdrwal[[1]],quantile$Remaining_value))
     })
+
   
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
  output$d0 <- renderDataTable({
    as.data.frame(df()[1]) 
@@ -84,20 +113,33 @@ server <- function(input,output){
  output$myhist <- renderPlot({
    ggplot(data = data.frame(df()[1]),aes(x=D0,fill="coral"))+
      geom_histogram()
-     
     })
+ 
+ output$remaining <- renderPlot({
+   df <- data.frame(df()[4])
+   colnames(df) <- c("remaining")
+   df$time <- seq(1:length(df$remaining))
+   ggplot(data = df,aes(y=remaining,x=time))+
+     geom_point()+
+     geom_line()+
+     geom_hline(yintercept = input$threshold,col="red")
+ })
  
  output$runtime <- renderText({paste(
    "Runtime is:",toString(df()[2])
    )
    })
 
- output$Mean <- renderText({paste(
-   "Mean of D0 is:",df()[3]
-   )
- })
 
-  
+
+ output$quantile <- renderText({paste(
+   "Quantile of D0 is:",toString(df()[3])
+ )
+ })
+ 
+ 
+ 
+ 
   
 }
 
